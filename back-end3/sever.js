9b3fc8041af66186e8bb7a694a978f24bf138c12
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2023-11-14 00:01:00 +0900

    Add Add nodemon Fix package.json Add img file in to ejs html

--- back-end3/sever.js
+++ back-end3/sever.js
@@ -1,18 +1,23 @@
 const express = require('express') //commonjs
+const path =  require('path');
+require('dotenv').config();
+
 //import express from 'express'; // es module
 
 const app = express() // app express
-require('dotenv').config();
 const port = process.env.PORT || 8081; // port => hardcode
 const hostname = process.env.HOST_NAME; 
 
+
 //config template engine
-app.set('views', './src/views/')
+app.set('views', path.join(__dirname,'src' ,'views'));
 app.set('view engine', 'ejs')
 
+//config static files
+app.use(express.static(path.join(__dirname , 'src' ,'public')));
 //khai bao route
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! with nodemon')
 })
 
 app.get('/home', (req, res) => {
