15b2de87794e8ab5ae8dc434a1a89a4c2e75591d
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2023-11-22 00:32:10 +0900

    Learning how to use MYSQL with nodejs. Install mysql2 into express Download docker to create a database. Add myqsl.yml compose declares information to create a database (container). Download DBeaver for operations, access and editing of the database. Add command in sever.js to connect and run database from MYSQL2

--- back-end3/sever.js
+++ back-end3/sever.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express') //commonjs
 const configViewEngine = require('./src/config/viewEngine');
 const webRoutes = require('./src/routes/web')
+const mysql = require('mysql2')
 //import express from 'express'; // es module
 
 const app = express() // app express
@@ -14,6 +15,27 @@ configViewEngine(app);
 
 //khai báo route
 app.use('/',webRoutes)
+
+
+//test connection 
+//create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'huynhnguyen'
+});
+
+connection.query(
+    'SELECT *FROM Users u',
+    function (err, results, fields) {
+        console.log('>>>results:',results);
+        console.log('>>>fields:',fields);
+    }
+)
+
+
 
 
 app.listen(port, hostname, () => {
