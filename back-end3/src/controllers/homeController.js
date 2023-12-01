1aac3416fe32680eaf6e92945a5848d387c51e2c
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2023-12-01 10:08:52 +0900

    Use contection query to push user data from client to SQL

--- back-end3/src/controllers/homeController.js
+++ back-end3/src/controllers/homeController.js
@@ -9,8 +9,22 @@ const getHomePage = (req, res) => {
 }
 
 const postCreateUser = (req, res) => {
-    console.log( '>>>> req.body: ', req.body);
-    res.send('Create a new user')
+    
+    
+    // let name = req.body.name;
+    // let email = req.body.email;
+    // let city = req.body.city;
+    
+    let {name, email, city} = req.body;
+    
+    let sql = `INSERT INTO Users (name, email, city) VALUES ('${name}', '${email}', '${city}')`;
+    connection.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log('>>>> result ', result);
+        res.send('Create a new user')
+    })
+
+    // res.send('Create a new user77777')
 }
 module.exports = {
     getLandingPage,
